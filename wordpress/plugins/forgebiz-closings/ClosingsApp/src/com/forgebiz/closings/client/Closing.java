1ff8772937c622b555c1172c177e575d6e304a32
Phillip Rhodes <rhodebump@gmail.com>
2018-03-06 12:06:37 -0500

    test
    test

--- wordpress/plugins/forgebiz-closings/ClosingsApp/src/com/forgebiz/closings/client/Closing.java
+++ wordpress/plugins/forgebiz-closings/ClosingsApp/src/com/forgebiz/closings/client/Closing.java
@@ -26,37 +26,73 @@ extends JavaScriptObject
   	return this.income_1;
   	}-*/;
     
-    public final native double getLabel2()/*-{
+    public final native double getIncome2()/*-{
   	return this.income_2;
   	}-*/;
     
-    public final native double getLabel3()/*-{
+    public final native double getIncome3()/*-{
   	return this.income_3;
   	}-*/;
     
-    public final native double getLabel4()/*-{
+    public final native double getIncome4()/*-{
   	return this.income__4;
   	}-*/;
     
-    public final native double getLabel5()/*-{
+    public final native double getIncome5()/*-{
   	return this.income_5;
   	}-*/;
     
-    public final native double getLabel6()/*-{
+    public final native double getIncome6()/*-{
   	return this.income_6;
   	}-*/;
     
-    public final native double getLabel7()/*-{
+    public final native double getIncome7()/*-{
   	return this.income_7;
   	}-*/;
     
-    public final native double getLabel8()/*-{
+    public final native double getIncome8()/*-{
   	return this.income_8;
   	}-*/;
     
-    public final native double getLabel9()/*-{
+    public final native double getIncome9()/*-{
   	return this.income_9;
   	}-*/;
     
+    public final native double getSales1()/*-{
+  	return this.sales_1;
+  	}-*/;
     
+    public final native double getSales2()/*-{
+  	return this.sales_2;
+  	}-*/;
+    
+    public final native double getSales3()/*-{
+  	return this.sales_3;
+  	}-*/;
+    
+    public final native double getSales4()/*-{
+  	return this.sales__4;
+  	}-*/;
+    
+    public final native double getSales5()/*-{
+  	return this.sales_5;
+  	}-*/;
+    
+    public final native double getSales6()/*-{
+  	return this.sales_6;
+  	}-*/;
+    
+    public final native double getSales7()/*-{
+  	return this.sales_7;
+  	}-*/;
+    
+    public final native double getSales8()/*-{
+  	return this.sales_8;
+  	}-*/;
+    
+    public final native double getSales9()/*-{
+  	return this.sales_9;
+  	}-*/;
+	
+	
 }
