90bf068ff77368be8cd3240ab7084ae59b402a41
Phillip Rhodes <rhodebump@gmail.com>
2018-03-06 12:10:21 -0500

    test2
    test

--- wordpress/plugins/forgebiz-closings/ClosingsApp/src/com/forgebiz/closings/client/Closing.java
+++ wordpress/plugins/forgebiz-closings/ClosingsApp/src/com/forgebiz/closings/client/Closing.java
@@ -93,6 +93,82 @@ extends JavaScriptObject
     public final native double getSales9()/*-{
   	return this.sales_9;
   	}-*/;
-	
+
+public final native void setSales1(double sales_1) /*-{
+  this.sales_1 = sales_1;
+  }-*/;
+  
+  public final native void setSales2(double sales_2) /*-{
+  this.sales_2 = sales_2;
+  }-*/;
+  
+  public final native void setSales3(double sales_3) /*-{
+  this.sales_3 = sales_3;
+  }-*/;
+  
+  public final native void setSales4(double sales_4) /*-{
+  this.sales_4 = sales_4;
+  }-*/;
+  
+  public final native void setSales5(double sales_5) /*-{
+  this.sales_5 = sales_5;
+  }-*/;
+  
+  public final native void setSales6(double sales_6) /*-{
+  this.sales_6 = sales_6;
+  }-*/;
+  
+  public final native void setSales7(double sales_7) /*-{
+  this.sales_7 = sales_7;
+  }-*/;
+  
+  public final native void setSales8(double sales_8) /*-{
+  this.sales_8 = sales_8;
+  }-*/;
+  
+  public final native void setSales9(double sales_9) /*-{
+  this.sales_9 = sales_9;
+  }-*/;
+  
+  public final native void setIncome1(double income_1) /*-{
+  this.income_1 = income_1;
+  }-*/;
+  
+  public final native void setIncome2(double income_2) /*-{
+  this.income_2 = income_2;
+  }-*/;
+  
+  public final native void setIncome3(double income_3) /*-{
+  this.income_3 = income_3;
+  }-*/;
+  
+  public final native void setIncome4(double income_4) /*-{
+  this.income_4 = income_4;
+  }-*/;
+  
+  public final native void setIncome5(double income_5) /*-{
+  this.income_5 = income_5;
+  }-*/;
+  
+  public final native void setIncome6(double income_6) /*-{
+  this.income_6 = income_6;
+  }-*/;
+  
+  public final native void setIncome7(double income_7) /*-{
+  this.income_7 = income_7;
+  }-*/;
+  
+  public final native void setIncome8(double income_8) /*-{
+  this.income_8 = income_8;
+  }-*/;
+  
+  public final native void setIncome9(double income_9) /*-{
+  this.income_9 = income_9;
+  }-*/;
+
+  
+  public final native int getId()/*-{
+	return this.id;
+	}-*/;
 	
 }
