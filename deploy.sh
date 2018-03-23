2b3c933d78fa7983ddd602f6d86341e4c016835a
rhodebump <rhodebump@gmail.com>
2018-03-22 21:21:43 -0400

    move to package plugin

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ant -f ./wordpress/plugins/forgebiz-closings/ClosingsApp/build.xml clean
 ant -f ./wordpress/plugins/forgebiz-closings/ClosingsApp/build.xml build
 
 #zip up the plugin
